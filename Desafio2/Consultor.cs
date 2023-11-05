2ea0ee16f6cf1e628b7862fe2c0b2faf16efddce
Bianca Ribeiro <132418587+biancafdribeiro@users.noreply.github.com>
2023-11-05 13:50:26 -0300

    Add files via upload

---
+++ Desafio2/Consultor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Desafio2
+{
+    public class Consultor
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataNascimento { get; set; }
+    }
+}
